0f89ff3e93b80b7a00b2b1d79a9fd41572d11e35
Haruka Takahashi <1124226@st.anan-nct.ac.jp>
2016-06-23 00:20:50 +0900

    shell script maded

--- MakingModel.py
+++ MakingModel.py
@@ -4,8 +4,9 @@ import logging
 # 進捗表示用
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-data = word2vec.Text8Corpus('tweets.txt')
-model = word2vec.Word2Vec(data, size=200)
+
+data = word2vec.Text8Corpus("data.txt")
+model = word2vec.Word2Vec(data, size=400)
 
 # 学習結果を出力する
 model.save("sample.model")
---
+++ Mecab_Wakati.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mecab -b 81920 -Owakati text.txt -o data.txt
+
