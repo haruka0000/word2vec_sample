157987068397684c332cb6152734d4237923907f
Haruka Takahashi <1124226@st.anan-nct.ac.jp>
2016-06-23 20:44:32 +0900

    xml to text

--- DbToText.py
+++ DbToText.py
@@ -3,17 +3,10 @@ import sqlite3
 def get_wiki():
   conn = sqlite3.connect("wikipedia.db")
   c = conn.cursor()
-  wiki_data = list(c.execute('''SELECT * FROM wiki;''')
+  wiki_data = list(c.execute('''SELECT * FROM wiki;''' ))[0]
   conn.commit()
   return wiki_data
 
-if __name__ == "__main__":
-  word = input(">>")
-  all_word = []
-  
-  # 書き込みモードで出力
-  f = open( "text.txt", "w" )
-  # すべてのtextから
-  for r in get_wiki(): 
-    f.write(r)  # textの書き出
-  f.close
+if __name__ == '__main__':
+	print(type(get_wiki()))
+	
